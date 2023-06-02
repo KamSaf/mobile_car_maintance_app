8d1b8c5dec7fd9b70cd127c422255de033e9b8be
bopaf <116653905+bopaf@users.noreply.github.com>
2023-06-02 13:46:17 +0200

    new activities (only gui): - add entry - entry content - entries list

---
+++ app/src/main/java/com/example/mobile_car_maintance_app/MaintanceEntriesActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobile_car_maintance_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaintanceEntriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintance_entries);
+    }
+}
