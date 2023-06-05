14ae2776a62e96c3a646eaabd6db43f3c3007540
bopaf <116653905+bopaf@users.noreply.github.com>
2023-06-05 13:27:08 +0200

    Implemented loggin into profile and creating profile.

--- app/src/main/java/com/example/mobile_car_maintance_app/CreatingProfile.java
+++ app/src/main/java/com/example/mobile_car_maintance_app/CreatingProfile.java
@@ -2,7 +2,13 @@ package com.example.mobile_car_maintance_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CreatingProfile extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class CreatingProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creating_profile);
+    }
+
+
+
+    public void createProfile(View v){
+        SQLiteDatabase myDB = openOrCreateDatabase("database.db", MODE_PRIVATE, null);
+        myDB.execSQL(
+                "CREATE TABLE IF NOT EXISTS profilesDatabase (id integer primary key, name VARCHAR(100))"
+        );
+        ContentValues profile = new ContentValues();
+        EditText inputName = findViewById(R.id.profileName);
+        profile.put("name", inputName.getText().toString());
+        try{
+            myDB.insert("profilesDatabase", null, profile);
+            Toast.makeText(this, "Pomyślnie utworzono profil.", Toast.LENGTH_SHORT).show();
+        }catch(Exception ex){
+            Toast.makeText(this, "Wystąpił błąd przy tworzeniu profilu.", Toast.LENGTH_SHORT).show();
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/mobile_car_maintance_app/ProfileContentActivity.java
+++ app/src/main/java/com/example/mobile_car_maintance_app/ProfileContentActivity.java
@@ -3,6 +3,8 @@ package com.example.mobile_car_maintance_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -14,6 +16,21 @@ public class ProfileContentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_content);
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        String loggedUserName = pref.getString("logged_user_name", "N/A");
+        TextView title = findViewById(R.id.TextView);
+        title.setText("Wybrany profil - "+loggedUserName);
+    }
+
+    public void deleteProfile(View v){
+        SQLiteDatabase myDB = openOrCreateDatabase("database.db", MODE_PRIVATE, null);
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        int loggedUserId = pref.getInt("logged_user_id", 999);
+
+        myDB.delete("profilesDatabase", "id=?", new String[]{String.valueOf(loggedUserId)});
+        myDB.close();
+        Intent intent = new Intent(this, ChooseProfileActivity.class);
+        startActivity(intent);
     }
 
     public void goToChoosingCar(View v) {
